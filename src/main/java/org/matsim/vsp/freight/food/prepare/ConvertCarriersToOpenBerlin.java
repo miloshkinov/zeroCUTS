7bbf0d87cc76ff8a282052bf749fda8f8b6656c6
kturner <martins-turner@vsp.tu-berlin.de>
2019-11-21 17:48:23 +0100

    continue work on CarrierConvertion

--- src/main/java/org/matsim/vsp/freight/food/prepare/ConvertCarriersToOpenBerlin.java
+++ src/main/java/org/matsim/vsp/freight/food/prepare/ConvertCarriersToOpenBerlin.java
@@ -31,82 +31,115 @@ class ConvertCarriersToOpenBerlin {
     private final String inputOldCarriers = "../../tubCloud/Shared/vsp_zerocuts/scenarios/Fracht_LEH-Shipments-Berlin/Input/I-Base_carrierLEH_v2_withFleet_Shipment.xml";
     private final String outputNewCarriers = "../../tubCloud/Shared/vsp_zerocuts/scenarios/Fracht_LEH_OpenBerlin/Input/I-Base_carrierLEH_v2_withFleet_Shipment.xml";
 
+	private Map<Id<Link>,Id<Link>> handledOldLinksToNewLink = new HashMap<>();
 
 	public static void main(String[] args) {
 		new ConvertCarriersToOpenBerlin().run();
 	}
 
 	private  void run(){
+		//TODO Konvertierung in einzelen Methoden auslagern(?)
 
 		//should be referenced in GK 4 after having a look at the net in via
-
 		Network openBerlinNet = NetworkUtils.readNetwork(inputNewNetwork);
-		Network oldSchroederNet = NetworkUtils.readNetwork(inputOldNet); 		//should be referenced in GK 4 after having a look at the net in via
+		Network oldNetwork = NetworkUtils.readNetwork(inputOldNet); 		//should be referenced in GK 4 after having a look at the net in via
 
 		Carriers oldCarriers = new Carriers();
 		CarrierPlanXmlReader carrierReader = new CarrierPlanXmlReader(oldCarriers);
 		carrierReader.readFile(inputOldCarriers);
 
-		Map<Id<Link>,Id<Link>> handledOldLinksToNewLink = new HashMap<>();
-		Map<Id<Link>, Carrier> handledNewLinksToCarrier = new HashMap<>();
-
+		//TODO add filter speed < 60 km/h -> nicht auf Autobahnen // vielleicht besser auf Link-eingeschaft.
 		NetworkFilterManager mng = new NetworkFilterManager(openBerlinNet);
 		mng.addLinkFilter(l -> {
 			return l.getAllowedModes().contains("car");
 		});
-		Network newNetOnlyCar = mng.applyFilters();
+		Network newNetworkFiltered = mng.applyFilters();
 		
 		Carriers newCarriers = new Carriers();
 
-		for(Carrier carrier: oldCarriers.getCarriers().values()){
-			Carrier newCarrier = CarrierUtils.createCarrier(carrier.getId());
+		for(Carrier oldCarrier: oldCarriers.getCarriers().values()){
+			Carrier newCarrier = CarrierUtils.createCarrier(oldCarrier.getId());
 			
-			//Capabilities TODO: implement with all details
-			CarrierCapabilities cc = carrier.getCarrierCapabilities();
-			log.error("CarrierCapabilities are not implemented");
+			//Capabilities
+			CarrierCapabilities cc = oldCarrier.getCarrierCapabilities();
+			newCarrier.getCarrierCapabilities().setFleetSize(cc.getFleetSize());
 			for (CarrierVehicle carrierVehicle: cc.getCarrierVehicles().values()) {
-				log.error("not implemented");
+				Id<Link> newLinkId;
+				newLinkId = getNewLinkId(oldNetwork, newNetworkFiltered, carrierVehicle.getLocation());
+				CarrierVehicle newCarrierVehicle = CarrierVehicle.Builder.newInstance(carrierVehicle.getId(), newLinkId)
+						.setTypeId(carrierVehicle.getVehicleTypeId())
+						.setEarliestStart(carrierVehicle.getEarliestStartTime())
+						.setLatestEnd(carrierVehicle.getLatestEndTime())
+						.build();
+				CarrierUtils.addCarrierVehicle(newCarrier, newCarrierVehicle);
+				//TODO Attributes
 			}
 			
 			//Services
-			//we only have services in this carriers file, no shipments
-			//TODO: look at this and work on it
-			for (CarrierService service : carrier.getServices().values()) {
-				Id<Link> newLink =handledOldLinksToNewLink.get(service.getLocationLinkId());
-				Coord oldLinkC = oldSchroederNet.getLinks().get(service.getLocationLinkId()).getCoord();
-				if(newLink == null) {
-					newLink = NetworkUtils.getNearestLink(newNetOnlyCar, oldLinkC).getId();
-					handledOldLinksToNewLink.put(service.getLocationLinkId(), newLink);
-				}
-
+			for (CarrierService service : oldCarrier.getServices().values()) {
+				Id<Link> newLinkId;
+				newLinkId = getNewLinkId(oldNetwork, newNetworkFiltered, service.getLocationLinkId());
 				CarrierService newService =
-							CarrierService.Builder.newInstance(Id.create(service.getId(), CarrierService.class), newLink)
+							CarrierService.Builder.newInstance(Id.create(service.getId(), CarrierService.class), newLinkId)
 							.setCapacityDemand(service.getCapacityDemand())
 							.setServiceDuration(service.getServiceDuration())
 							.setServiceStartTimeWindow(service.getServiceStartTimeWindow())
 							.build();
-					newCarrier.getServices().put(newService.getId(), newService);
-					handledNewLinksToCarrier.put(newLink, newCarrier);
+				for (String attKey : service.getAttributes().getAsMap().keySet()){
+					newService.getAttributes().putAttribute(attKey, service.getAttributes().getAttribute(attKey));
+				}
+				CarrierUtils.addService(newCarrier, newService);
 				}
 			
-			//Shipments TODO: implement
-			for (CarrierShipment shipment : carrier.getShipments().values()) {
-				log.error("not implemented");
+			//Shipments
+			for (CarrierShipment shipment : oldCarrier.getShipments().values()) {
+				Id<Link> newLinkIdFrom;
+				Id<Link> newLinkIdTo;
+				newLinkIdFrom = getNewLinkId(oldNetwork, newNetworkFiltered, shipment.getFrom());
+				newLinkIdTo = getNewLinkId(oldNetwork, newNetworkFiltered, shipment.getTo());
+				CarrierShipment newShipment =
+						CarrierShipment.Builder.newInstance(Id.create(shipment.getId(), CarrierShipment.class),newLinkIdFrom, newLinkIdTo, shipment.getSize())
+								.setPickupServiceTime(shipment.getPickupServiceTime())
+								.setDeliveryServiceTime(shipment.getDeliveryServiceTime())
+								.setPickupTimeWindow(shipment.getPickupTimeWindow())
+								.setDeliveryTimeWindow(shipment.getDeliveryTimeWindow())
+								.build();
+				for (String attKey : shipment.getAttributes().getAsMap().keySet()){
+					newShipment.getAttributes().putAttribute(attKey, shipment.getAttributes().getAttribute(attKey));
+				}
+				CarrierUtils.addShipment(newCarrier, newShipment);
 			}
 			
-			//Attributes
-			Attributes attributes = carrier.getAttributes();
-			for (String att_Key : attributes.getAsMap().keySet()){
-				newCarrier.getAttributes().putAttribute(att_Key, attributes.getAttribute(att_Key));
+			//Carrier attributes
+			Attributes attributes = oldCarrier.getAttributes();
+			for (String attKey : attributes.getAsMap().keySet()){
+				newCarrier.getAttributes().putAttribute(attKey, attributes.getAttribute(attKey));
+			}
+
+			if (oldCarrier.getPlans().size() > 0) {
+				log.warn("Please note, that the existing tours will not be available in the converted file");
 			}
 			
-			log.warn("Please note, that the tours are not available in the converted file");
-			
-			newCarriers.addCarrier(carrier);
+			newCarriers.addCarrier(newCarrier);
 		}
-		
+
+
 		new CarrierPlanXmlWriterV2(newCarriers).write(outputNewCarriers);
 
+		//TODO: Überprüfe ob Anzahl Vehicle, Services und Shipments korrekt ist.
+		log.info("#### Done ####");
+	}
+
+	private Id<Link> getNewLinkId(Network oldNetwork, Network newNetwork, Id<Link> oldLinkId) {
+		Id<Link> newLinkId;
+		if(!handledOldLinksToNewLink.containsKey(oldLinkId)) {
+			Coord oldLinkCoord = oldNetwork.getLinks().get(oldLinkId).getCoord();
+			newLinkId = NetworkUtils.getNearestLink(newNetwork, oldLinkCoord).getId();
+			handledOldLinksToNewLink.put(oldLinkId, newLinkId);
+		} else{
+			newLinkId =handledOldLinksToNewLink.get(oldLinkId);
+		}
+		return newLinkId;
 	}
 
 }
