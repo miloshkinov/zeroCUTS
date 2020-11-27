81280ccc408d1e70be7ee215cff9ecad8c73d375
rewert <ewert@vsp.tu-berlin.de>
2020-11-27 09:51:08 +0100

    initial commit

---
+++ src/main/java/org/matsim/vsp/demandGeneration/GeneralDemandGeneration.java
@@ -0,0 +1,54 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.vsp.demandGeneration; 
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import com.google.common.base.Joiner;
+
+
+/**
+ * @author: rewert
+ * TODO
+ */
+
+public class GeneralDemandGeneration {
+
+	private static final Logger log = LogManager.getLogger(GeneralDemandGeneration.class);
+
+	private static final String inputNetwork = "";
+	private static final String outputFolder = "";
+
+	public static void main(String[] args) {
+		//TODO
+	}
+
+	
+}
