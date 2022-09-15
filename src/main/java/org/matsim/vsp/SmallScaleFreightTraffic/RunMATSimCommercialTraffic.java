1d2e2b1e17b5b0776b990aff771adc0d9fd5efdc
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2022-09-15 10:21:51 +0200

    add longDistanceFreight to generated traffic

--- src/main/java/org/matsim/vsp/SmallScaleFreightTraffic/RunMATSimCommercialTraffic.java
+++ src/main/java/org/matsim/vsp/SmallScaleFreightTraffic/RunMATSimCommercialTraffic.java
@@ -20,12 +20,14 @@
 package org.matsim.vsp.SmallScaleFreightTraffic;
 
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Activity;
@@ -43,6 +45,7 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.ScoringFunctionFactory;
@@ -52,6 +55,10 @@ import org.matsim.core.scoring.functions.CharyparNagelAgentStuckScoring;
 import org.matsim.core.scoring.functions.CharyparNagelLegScoring;
 import org.matsim.core.scoring.functions.ScoringParameters;
 import org.matsim.core.scoring.functions.ScoringParametersForPerson;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+
 import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import picocli.CommandLine;
 
@@ -92,10 +99,11 @@ public class RunMATSimCommercialTraffic implements Callable<Integer> {
 		config.qsim().setFlowCapFactor(inputScale);
 		config.qsim().setStorageCapFactor(inputScale);
 		config.qsim().setPcuThresholdForFlowCapacityEasing(0.5);
-		config.vehicles().setVehiclesFile("berlin_bothTypes_5pct_allVehicles.xml.gz");
-		config.plans().setInputFile("berlin_bothTypes_5pct_plans.xml.gz");
+		config.vehicles().setVehiclesFile("berlin_bothTypes_" + (int) (inputScale * 100) + "pct_allVehicles.xml.gz");
+		config.plans().setInputFile("berlin_bothTypes_" + (int) (inputScale * 100) + "pct_plans.xml.gz");
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
+		addLongDistanceFreightTraffic(scenario);
 		createActivityParams(scenario);
 
 		Controler controler = prepareControler(scenario);
@@ -103,7 +111,7 @@ public class RunMATSimCommercialTraffic implements Callable<Integer> {
 		controler.addOverridingModule(new CadytsCarModule());
 
 		// include cadyts into the plan scoring (this will add the cadyts corrections to
-		// the scores):
+		// the scores)
 		controler.setScoringFunctionFactory(new ScoringFunctionFactory() {
 			@Inject
 			CadytsContext cadytsContext;
@@ -134,40 +142,80 @@ public class RunMATSimCommercialTraffic implements Callable<Integer> {
 		return 0;
 	}
 
-	/** Create Activity parameter for planCalcScore.
+	private void addLongDistanceFreightTraffic(Scenario scenario) {
+
+		Population longDistanceFreightPopulation = PopulationUtils.readPopulation(
+				inputPath.resolve("berlin_longDistanceFreight_" + (int) (inputScale * 100) + "pct.xml.gz").toString());
+
+		longDistanceFreightPopulation.getPersons().values().forEach(
+				person -> VehicleUtils.insertVehicleIdsIntoAttributes(person, (new HashMap<String, Id<Vehicle>>() {
+					{
+						put("freight", Id.createVehicleId(person.getId().toString()));
+					}
+				})));
+		longDistanceFreightPopulation.getPersons().values()
+				.forEach(person -> scenario.getVehicles().addVehicle(VehicleUtils.createVehicle(
+						Id.createVehicleId(person.getId().toString()),
+						scenario.getVehicles().getVehicleTypes().get(Id.create("medium18t", VehicleType.class)))));
+		longDistanceFreightPopulation.getPersons().values()
+				.forEach(person -> scenario.getPopulation().addPerson(person));
+		
+		log.info(longDistanceFreightPopulation.getPersons().size() + " Agents for the long distance freight added");
+	}
+
+	/**
+	 * Create Activity parameter for planCalcScore.
+	 * 
 	 * @param scenario
 	 */
 	private void createActivityParams(Scenario scenario) {
 		Population population = scenario.getPopulation();
 		Config config = scenario.getConfig();
 		int i = 0;
+		int countPersons = 0;
 		for (Person person : population.getPersons().values()) {
+			countPersons++;
+			if (countPersons % 1000 == 0)
+				log.info("Activities for "+ countPersons + " of " + population.getPersons().size() + " persons generated.");
 			double tourStartTime = 0;
 			for (PlanElement planElement : person.getSelectedPlan().getPlanElements()) {
 				if (planElement instanceof Activity) {
 					i++;
 					String newTypeName = ((Activity) planElement).getType().toString() + "_" + i;
 					((Activity) planElement).setType(newTypeName);
-					if (newTypeName.contains("service"))
+					if (newTypeName.contains("service")) {
 						config.planCalcScore()
 								.addActivityParams(new ActivityParams(newTypeName)
 										.setTypicalDuration(((Activity) planElement).getMaximumDuration().seconds())
 										.setOpeningTime(tourStartTime).setClosingTime(tourStartTime + 8. * 3600.));
-					if (newTypeName.contains("start")) {
+						continue;
+					}
+					if (newTypeName.contains("start") && !newTypeName.contains("freight")) {
 						tourStartTime = ((Activity) planElement).getEndTime().seconds();
-						config.planCalcScore().addActivityParams(
-								new ActivityParams(newTypeName).setTypicalDuration(1).setOpeningTime(6. * 3600.)
+						config.planCalcScore()
+								.addActivityParams(new ActivityParams(newTypeName).setOpeningTime(6. * 3600.)
 										.setClosingTime(20. * 3600.).setLatestStartTime(tourStartTime)
 										.setEarliestEndTime(6. * 3600.).setTypicalDuration(5 * 60));
+						continue;
 					}
-					if (newTypeName.contains("end")) {
+					if (newTypeName.contains("end") && !newTypeName.contains("freight")) {
 						double tourEndTime = tourStartTime + 9 * 3600;
 						if (tourEndTime > 24 * 3600)
 							tourEndTime = 24 * 3600;
 						config.planCalcScore()
-								.addActivityParams(new ActivityParams(newTypeName).setTypicalDuration(1)
+								.addActivityParams(new ActivityParams(newTypeName)
 										.setOpeningTime(tourStartTime + 6 * 3600).setClosingTime(tourEndTime)
 										.setLatestStartTime(tourEndTime).setTypicalDuration(5 * 60));
+						continue;
+					}
+					if (newTypeName.contains("freight_start")) {
+						tourStartTime = ((Activity) planElement).getEndTime().seconds();
+						config.planCalcScore().addActivityParams(new ActivityParams(newTypeName).setTypicalDuration(1));
+						continue;
+					}
+					if (newTypeName.contains("freight_end")) {
+						config.planCalcScore().addActivityParams(new ActivityParams(newTypeName).setTypicalDuration(1));
+						continue;
 					}
 				}
 			}
