20fee6983b6cb95b3a59fb8d4daee2f14999854a
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2023-03-22 20:17:56 +0100

    add network CRS as input

--- src/main/java/org/matsim/vsp/freightAnalysis/RunAnalyse.java
+++ src/main/java/org/matsim/vsp/freightAnalysis/RunAnalyse.java
@@ -2,6 +2,7 @@ package org.matsim.vsp.freightAnalysis;
 
 import java.io.IOException;
 
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.io.UncheckedIOException;
 
 public class RunAnalyse {
@@ -9,14 +10,15 @@ public class RunAnalyse {
 	public static void main(String[] args) throws UncheckedIOException, IOException {
 		
 //		String runDir = "C:/Users/Ricardo/git/zeroCUTS/output/BusinessPassengerTraffic/leipzig/commercialTraffic_0.1pct_2023-03-03_43939_0.005/";
-		String runDir = "C:/Users/Ricardo/Desktop/SoundingBoard/commercialTraffic_20pct_2023-03-06_44655_0.005/";
+		String runDir = "C:/Users/Ricardo/Desktop/SoundingBoard/businessTraffic_5pct_2023-03-07_48622_0.005/";
+//		String runId = "tax40CV.";
+		String networkCRS = TransformationFactory.DHDN_GK4;
 		String runId = "base.";
-//		String runId = null;
 
 		if (runId != null)
-			FreightAnalyse.main(new String[] { runDir, "true", runId });
+			FreightAnalyse.main(new String[] { runDir, "true", networkCRS, runId });
 		else
-			FreightAnalyse.main(new String[] { runDir, "true"});
+			FreightAnalyse.main(new String[] { runDir, "true", networkCRS});
 
 	}
 
