9f718491bff1d28dfeb08b138961036fe294fb6f
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2024-02-16 23:36:25 +0100

    run for all DC/non-DC runs

--- src/main/java/org/matsim/vsp/freight/food/UpdateRunOutputs.java
+++ src/main/java/org/matsim/vsp/freight/food/UpdateRunOutputs.java
@@ -1,12 +1,15 @@
 package org.matsim.vsp.freight.food;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.freight.carriers.Carrier;
@@ -16,6 +19,8 @@ import org.matsim.freight.carriers.CarriersUtils;
 import org.matsim.freight.carriers.FreightCarriersConfigGroup;
 import org.matsim.freight.carriers.ScheduledTour;
 import org.matsim.freight.carriers.Tour;
+import org.matsim.freight.carriers.analysis.RunFreightAnalysisEventBased;
+import org.matsim.freight.carriers.controler.CarrierModule;
 
 /**
  * Diese Klasse soll den Output von "alten" runs derart updaten,
@@ -26,20 +31,41 @@ import org.matsim.freight.carriers.Tour;
 public class UpdateRunOutputs {
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-    run(args, false);
+    var listOfRuns = List.of(
+        "foodRetailing_wo_rangeConstraint/71_ICEVBEV_NwCE_BVWP_10000it_DCoff_noTax/",
+        "foodRetailing_wo_rangeConstraint/71a_ICEV_NwCE_BVWP_10000it_DCoff_noTax/",
+        "foodRetailing_wo_rangeConstraint/72_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax25/",
+        "foodRetailing_wo_rangeConstraint/73_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax50/",
+        "foodRetailing_wo_rangeConstraint/74_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax100/",
+        "foodRetailing_wo_rangeConstraint/75_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax150/",
+        "foodRetailing_wo_rangeConstraint/76_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax200/",
+        "foodRetailing_wo_rangeConstraint/77_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax250/",
+        "foodRetailing_wo_rangeConstraint/78_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax300/",
+        //nun Runs mit ReichweitenConstraint
+        "foodRetailing_with_rangeConstraint/21_ICEVBEV_NwCE_BVWP_10000it_DCoff_noTax/",
+        "foodRetailing_with_rangeConstraint/22_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax25/",
+        "foodRetailing_with_rangeConstraint/23_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax50/",
+        "foodRetailing_with_rangeConstraint/24_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax100/",
+        "foodRetailing_with_rangeConstraint/25_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax150/",
+        "foodRetailing_with_rangeConstraint/26_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax200/",
+        "foodRetailing_with_rangeConstraint/27_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax250/",
+        "foodRetailing_with_rangeConstraint/28_ICEVBEV_NwCE_BVWP_10000it_DCoff_Tax300/"
+    )   ;
+    for (String runDir : listOfRuns) {
+        run(args, runDir);
+    }
   }
-  public static void run( String[] args, boolean runWithOTFVis )
+  public static void run( String[] args, String runDir )
       throws InterruptedException, ExecutionException {
 
     // Path to public repo:
-//    String pathToInput = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/freight/foodRetailing_wo_rangeConstraint/input/";
-    String pathToInput = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/freight/foodRetailing_wo_rangeConstraint/71_ICEVBEV_NwCE_BVWP_10000it_DCoff_noTax/";
+    String pathToInput = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/freight/" + runDir;
     // ### config stuff: ###
     Config config;
     if ( args==null || args.length==0 || args[0]==null ){
       config = ConfigUtils.createConfig();
       config.network().setInputFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_network.xml.gz");
-      config.controller().setOutputDirectory( "./output/freightUpdate71-1" );
+      config.controller().setOutputDirectory( "../shared-svn/projects/freight/studies/UpdateEventsfromEarlierStudies/" + runDir );
       config.controller().setLastIteration( 0 );  // no iterations; for iterations see RunFreightWithIterationsExample.  kai, jan'23
       config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
       config.global().setCoordinateSystem("EPSG:31468");
@@ -57,8 +83,8 @@ public class UpdateRunOutputs {
     //load carriers according to freight config
     CarriersUtils.loadCarriersAccordingToFreightConfig( scenario );
 
-    // output before jsprit run (not necessary)
-    new CarrierPlanWriter(CarriersUtils.getCarriers( scenario )).write( "output/unmodifiedCarriers.xml" ) ;
+    // output before jsprit runDir (not necessary)
+//    new CarrierPlanWriter(CarriersUtils.getCarriers( scenario )).write( "output/unmodifiedCarriers.xml" ) ;
     // (this will go into the standard "output" directory.  note that this may be removed if this is also used as the configured output dir.)
 
     for (Carrier carrier : CarriersUtils.addOrGetCarriers(scenario).getCarriers().values()) {
@@ -82,22 +108,22 @@ public class UpdateRunOutputs {
       selectedPlan.setScore(Double.NEGATIVE_INFINITY);
     }
 
-    new CarrierPlanWriter(CarriersUtils.getCarriers( scenario )).write( "output/updatedCarriersWithTourIds.xml" );
-//
-//    // ## MATSim configuration:  ##
-//    final Controler controler = new Controler( scenario ) ;
-//    controler.addOverridingModule(new CarrierModule() );
-//
-//
-//    // ## Start of the MATSim-Run: ##
-//    controler.run();
+//    new CarrierPlanWriter(CarriersUtils.getCarriers( scenario )).write( "output/updatedCarriersWithTourIds.xml" );
 
-//    var analysis = new RunFreightAnalysisEventBased(config.controller().getOutputDirectory()+"/", config.controller().getOutputDirectory()+"/analysis", "EPSG:31468");
-//    try {
-//      analysis.runAnalysis();
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
+    // ## MATSim configuration:  ##
+    final Controler controler = new Controler( scenario ) ;
+    controler.addOverridingModule(new CarrierModule() );
+
+
+    // ## Start of the MATSim-Run: ##
+    controler.run();
+
+    var analysis = new RunFreightAnalysisEventBased(config.controller().getOutputDirectory() , config.controller().getOutputDirectory()+"Analysis", "EPSG:31468");
+    try {
+      analysis.runAnalysis();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
